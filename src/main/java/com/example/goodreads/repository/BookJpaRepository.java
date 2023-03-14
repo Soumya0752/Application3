6a21010c84a264477d07212bc6be7defd5d402c9
Soumya0752 <esampallysoumya123@gmail.com>
2023-03-14 01:35:53 +0000

    solution

---
+++ src/main/java/com/example/goodreads/repository/BookJpaRepository.java
@@ -0,0 +1,9 @@
+package com.example.goodreads.repository;
+import com.example.goodreads.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository; 
+
+@Repository
+public interface BookJpaRepository extends JpaRepository<Book, Integer> {
+   
+
+}
